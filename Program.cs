7b04612d8e3ea6092eb666dfa11c0fa7a3d93442
Lance Frederick Bueza (1000121145) <BUEZLF1@student.op.ac.nz>
2023-05-11 10:27:48 +1200

    creating project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GPOUP2NEWPROJECT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("my name is Lance")
+        }
+    }
+}
